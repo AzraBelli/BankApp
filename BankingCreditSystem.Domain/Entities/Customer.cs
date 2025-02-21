4aafc745b4ca058763679265d78e181f83f9430b
AzraNurMISIRBELLI <77413791+AzraNurMISIRBELLI@users.noreply.github.com>
2025-02-20 21:51:52 -0600

    first commit

---
+++ BankingCreditSystem.Domain/Entities/Customer.cs
@@ -0,0 +1,12 @@
+public abstract class Customer : Entity<Guid>
+{
+    public string CustomerNumber { get; set; } = default!;
+    public string PhoneNumber { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Address { get; set; } = default!;
+    public bool IsActive { get; set; };
+    protected Customer()
+    {
+        IsActive = true;
+    }
+} 
