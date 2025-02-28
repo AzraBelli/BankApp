0fdb9616c2bafd4533e09e10bef7dcda28d61059
AzraBELLI <77413791+AzraBELLI@users.noreply.github.com>
2025-02-28 05:42:20 -0600

    Controller and Service Registration Configuration, Added Dependency Injection

--- BankingCreditSystem.WebApi/Program.cs
+++ BankingCreditSystem.WebApi/Program.cs
@@ -1,8 +1,18 @@
+using BankingCreditSystem.Core.CrossCuttingConcerns.Exceptions.Middlewares;
+using BankingCreditSystem.Application;
+using BankingCreditSystem.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();  
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -10,11 +20,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 
 app.UseHttpsRedirection();
 app.UseCustomExceptionMiddleware();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
 
